85910e26967975461b9e3a92ade2645e2777de74
Dan Atanasov <danmanm@gmail.com>
2025-11-07 17:45:50 +0200

    Enhance Neutral page with additional context on gameplay dynamics and strategies

--- app/mortal-kombat-1/advanced-concepts/neutral/page.tsx
+++ app/mortal-kombat-1/advanced-concepts/neutral/page.tsx
@@ -22,10 +22,31 @@ export default async function Page() {
       <p className="text-green-500">Screenshot of typical round start.</p>
 
       <p>
-        All game rounds start in Neutral state. If it is the first round,
-        fighters are positioned at the center of the arena with the default
-        space between them. Subsequent rounds depend on where the previous one
-        ended. In any case, both combatants are some distance apart.
+        To give you an idea of how this looks,{' '}
+        <strong>all game rounds start in Neutral</strong> state. If it is the
+        first round, fighters are positioned at the center of the arena with the
+        default space between them. Subsequent rounds depend on where the
+        previous one ended. In any case, both combatants start some distance
+        apart.
+      </p>
+
+      <p>
+        From here play can continue in any direction, but so far as no advantage
+        is gained by either character, the state remains Neutral. Players can
+        back away or come closer, dash, jump around or over each other, probe
+        with safe attacks and this can go on for some time. Eventually one of
+        them will take a risk trying and gain the upper hand.
+      </p>
+
+      <p className="text-lg font-bold">
+        The goal of Neutral is to gain a lead in the fight.
+      </p>
+
+      <p>
+        It can be done through baiting a mistake from your foe, pressuring them
+        into one or patiently waiting for an opportunity. Timing and reaction
+        play as much a role as understanding the characters. Here are some terms
+        and tools that can clarify Neutral.
       </p>
 
       <h2>Footsies</h2>
