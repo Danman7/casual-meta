dc0bd0219acbf78c07b21b79e76837101e2a5383
Dan Atanasov <danmanm@gmail.com>
2025-11-07 16:38:10 +0200

    Update h3 font size and enhance Neutral page with additional descriptions and concepts

--- app/mortal-kombat-1/advanced-concepts/neutral/page.tsx
+++ app/mortal-kombat-1/advanced-concepts/neutral/page.tsx
@@ -18,6 +18,24 @@ export default async function Page() {
         <strong>neither fighter has advantage over the other</strong>. Both are
         free to move, attack or block as they please.
       </p>
+
+      <p className="text-green-500">Screenshot of typical round start.</p>
+
+      <p>
+        All game rounds start in Neutral state. If it is the first round,
+        fighters are positioned at the center of the arena with the default
+        space between them. Subsequent rounds depend on where the previous one
+        ended. In any case, both combatants are some distance apart.
+      </p>
+
+      <h2>Footsies</h2>
+      <p>
+        One or both players can choose to rush down and press the attack
+        immediately. Alternatively, they can start to move forwards and
+        backwards, in and out of range of their opponent’s anticipated attacks
+        without committing an attack. This is often referred to as{' '}
+        <strong>Footsies</strong>.
+      </p>
     </article>
   )
 }
