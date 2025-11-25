01eb31f1179addf5c48c4b1429a16533104aff23
Dan Atanasov <danmanm@gmail.com>
2025-11-25 12:59:11 +0200

    feat: streamline Scorpion's normals descriptions for clarity and conciseness

--- app/mortal-kombat-1/scorpion/page.tsx
+++ app/mortal-kombat-1/scorpion/page.tsx
@@ -1,7 +1,6 @@
 import Image from 'next/image'
-import { FaGraduationCap, FaPersonArrowUpFromLine } from 'react-icons/fa6'
+import { FaGraduationCap } from 'react-icons/fa6'
 import {
-  GiAirplaneArrival,
   GiArmorPunch,
   GiBrokenShield,
   GiHighKick,
@@ -11,10 +10,9 @@ import {
   GiStairsGoal,
   GiThumbUp,
   GiThunderball,
-  GiWingfoot,
 } from 'react-icons/gi'
 import { IoWarning } from 'react-icons/io5'
-import { LuBetweenHorizontalEnd, LuShieldCheck } from 'react-icons/lu'
+import { LuBetweenHorizontalEnd } from 'react-icons/lu'
 import {
   MdAltRoute,
   MdOutlineSocialDistance,
@@ -22,7 +20,6 @@ import {
 } from 'react-icons/md'
 import { PiArrowsCounterClockwiseBold } from 'react-icons/pi'
 import { RiBreadFill } from 'react-icons/ri'
-import { TbRuler3, TbRulerMeasure } from 'react-icons/tb'
 import { TiArrowShuffle } from 'react-icons/ti'
 
 import Scorpion from '@/app/assets/mk1/scorp_mk1.webp'
@@ -148,137 +145,64 @@ export default async function Page() {
 
       <h2>Key Normals</h2>
 
-      <h3>
-        Forward 32{' '}
-        <FlexWrapper className="inline-flex text-base">
-          <Badge className="text-success border-success">
-            <LuShieldCheck /> Safe (-6 on block)
-          </Badge>
-
-          <Badge className="text-success border-success">
-            <FaPersonArrowUpFromLine /> Launching combo starter
-          </Badge>
-
-          <Badge>12f start</Badge>
-        </FlexWrapper>
-      </h3>
-
       <p>
-        This is Scorpion’s launching mid-high combo starter. It is reasonably
-        fast, but even while advancing it has a short reach. It is best employed
-        after a miss or an unsafe attack from your opponent. Connecting out of
-        neutral without the help of the spear is preferable as you will find
-        extending combos later easier.
+        <strong>Forward 32</strong> is Scorpion’s{' '}
+        <strong>top starter in terms of combo potential</strong>. It is a{' '}
+        <strong className="text-success">safe</strong> (-6 on block),{' '}
+        <strong>reasonably quick</strong> (12 frames start) mid-high string that{' '}
+        <strong className="text-success">launches</strong> the opponent in the
+        air. Naturally, due to gravity scaling, it is best utilized towards the
+        beginning of the combo. Scorpion can juggle his foe up to three times in
+        a row and still have enough height for a standing 4 kick into a spear.
+        Or, he can juggle two times instead and transition into an aerial combo
+        using J12 exJDB2 111.
       </p>
 
       <p>
-        <strong>F32 can be repeated up to three times in a row</strong>,
-        depending on gravity scaling. Naturally doing it towards the start of a
-        combo will have better results.
-      </p>
-
-      <Callout>
-        <div className="text-base font-bold flex items-center gap-2">
-          <GiHighKick /> Practice
-        </div>
-        <p>
-          You can start practicing three F32s in a row immediately. The last one
-          will give you little height, but still enough to connect a standing 4.
-          Try <strong>F32 F32 F32 4 BF1</strong>. Pressing standing 4 instead of
-          forward 4 (which will not connect) will be the most difficult part but
-          will greatly extend your combo potential.
-        </p>
-
-        <p>
-          Alternatively, you can chain only two F32s into a common Scorpion one
-          bar aerial extension: <strong>F32 F32 J12 exDB2 111</strong>. This is
-          a bit more demanding, but if you take it just a step further and pause
-          between the dxDB2 and 111 so you hit closer to the ground, you can
-          even connect a spear afterwards.
-        </p>
-      </Callout>
-
-      <h3>Standing 21</h3>
-
-      <FlexWrapper className="mt-2">
-        <Badge className="text-success border-success">
-          <LuShieldCheck /> Safe (-2 on block)
-        </Badge>
-
-        <Badge className="text-success border-success">
-          <GiWingfoot /> Fast (10f start)
-        </Badge>
-
-        <Badge className="text-success border-success">
-          <TbRuler3 /> Decent Reach
-        </Badge>
-
-        <Badge className="text-success border-success">
-          <GiJoint /> Disjointed
-        </Badge>
-      </FlexWrapper>
-
-      <p>
-        This is your <strong>primary challenge and hit-confirm</strong>. It has
-        surprising reach making it a preferable combo starter over the 12,
-        unless you are touching with the enemy and speed is of the essence.
+        <strong>Standing 21</strong> is Scorpions’s{' '}
+        <strong>primary mid-range challenge and hit-confirm</strong>. It is also
+        his most consistent combo starter. It is a{' '}
+        <strong className="text-success">fast</strong> (10 frames start),{' '}
+        <strong className="text-success">safe</strong> (-2 on block),{' '}
+        <strong className="text-success">disjointed</strong> high string that
+        also has a <strong className="text-success">surprising reach</strong>.
+        Practice, measuring it. It is second to F32 in combo potential because
+        if you hit-confirm you can only cancel into a spear or a Kameo move,
+        limiting your extension later on.
       </p>
 
       <p>
-        Initiating a combo with 21 is easier than F32, but you will have to
-        cancel into spear right away unless your Kameo can extend.
+        <strong>Back 3</strong> is a very singular normal. Scorpion sweeps his
+        Kunai for a <strong className="text-error">slow</strong> (21 frames
+        start), <strong className="text-error">unsafe</strong> (-19 on block),
+        but very <strong className="text-success">far reach</strong> (jump
+        distance plus a dash){' '}
+        <strong className="text-success">disjointed</strong> attack that makes
+        two hits - a mid, followed by a low one. It is among the longest
+        reaching normals in the game. Even as slow and predictable as it is, the
+        threat of a low attack at such a distance pushes the opponent to
+        constantly be ready to block low or jump over it.
+      </p>
+
+      <p className="font-bold">
+        Also, B3’s first hit is cancelable into any special or Kameo move.
+      </p>
+
+      <p>
+        <strong>Back 2</strong> is a{' '}
+        <strong className="text-success">safe</strong> (0 on block),{' '}
+        <strong className="text-success">fast</strong> (9 frames start) high
+        attack with
+        <strong className="text-success">good reach</strong> that is also a
+        decent <strong className="text-success">anti-air</strong> defense. It
+        also can be cancelled into any special or Kameo move on hit. You can
+        bait a jump out of your foe with the B3 into a timed B2 interrupt into
+        spear.
       </p>
 
       <Callout>
         <div className="text-base font-bold flex items-center gap-2">
-          <GiHighKick /> Practice
-        </div>
-        <p>
-          Practice 21 BF1. Record a dummy walking forward and then dashing
-          forward. Learn to measure the distance at which standing 2 connects.
-          Extend it with 33 DB2 or 333 BF2 after the spear.
-        </p>
-      </Callout>
-
-      <h3>Sweeping Tail - B3</h3>
-      <FlexWrapper className="mt-2">
-        <Badge className="text-success border-success">
-          <GiJoint /> Disjointed
-        </Badge>
-
-        <Badge className="text-success border-success">
-          <TbRulerMeasure /> Very long reach
-        </Badge>
-
-        <Badge className="text-success border-success">First hit cancels</Badge>
-
-        <Badge className="text-error border-error!">
-          <GiSnail /> Slow (21f start)
-        </Badge>
-
-        <Badge className="text-error border-error!">
-          <GiBrokenShield /> Unsafe (-19 on block)
-        </Badge>
-      </FlexWrapper>
-
-      <p>
-        Scorpion’s Back 3 is distinctive. He uses his Kunai for a disjointed
-        attack that makes two hits - a mid followed by a low one. It extends
-        over at jump distance plus one dash, making it one of the furthest
-        reaching normal in the game. Even as slow and predictable as it is, the
-        threat of a low attack at such a distance conditions the other player to
-        block low or jump over it.
-      </p>
-
-      <p>
-        Also, importantly, the first mid hit of Back 3 cancels into any special
-        or Kameo move, providing interesting pressure alternatives. Just don’t
-        try it often at close range.
-      </p>
-
-      <Callout>
-        <div className="text-base font-bold flex items-center gap-2">
-          Back 3 Cancels
+          Back forward cancels
         </div>
 
         <p>
@@ -287,72 +211,31 @@ export default async function Page() {
         </p>
 
         <p>
-          <strong>B3 F1</strong> cancels into Spear.
+          <strong>B2 F1</strong> cancels into spear for a nice full combo out of
+          anti-air.
         </p>
         <p>
-          <strong>B3 F2</strong> cancels into Dash.
+          <strong>B2 F2</strong> cancels into dash which is chainable only with
+          certain Kameo moves.
         </p>
 
         <p>
           <strong>B3 exF4</strong> cancels into enhanced Hellfire. The enhanced
-          version is guaranteed damage even if they block. It is a safe chip
-          resource.
+          version is guaranteed damage even if they block. This is a moderately
+          safe chip damage resource (if they block).
         </p>
 
         <p>
           <strong>B3 exDB3</strong> cancels into Enhanced Teleport. This is an
           excellent way to force them into blocking, giving your teleport more
           freedom. Be careful - it can still be interrupted but requires
-          pinpoint accuracy. Try B3 exDB3 12 BF1. Try recording the dummy to
-          block at random intervals. With some practice you can learn when to
-          try and connect the punch and when to throw.
+          pinpoint accuracy. Try B3 exDB3 12 BF1 if they take the hit or B3
+          exDB3 throw if they block.
         </p>
 
         <p>
           As mentioned, Scorpion has a poor mix-up without Kameos. The B3 cancel
           is a good candidate for Kameo synergy.
-        </p>
-      </Callout>
-
-      <h3>Swiping Kunai - B2</h3>
-
-      <FlexWrapper className="mt-2">
-        <Badge className="text-success border-success">
-          <LuShieldCheck /> Safe (0 on block)
-        </Badge>
-
-        <Badge className="text-success border-success">
-          <GiWingfoot /> Fast (9f start)
-        </Badge>
-
-        <Badge className="text-success border-success">
-          <GiAirplaneArrival /> Anti-air
-        </Badge>
-
-        <Badge className="text-success border-success">
-          <TbRuler3 /> Decent Reach
-        </Badge>
-      </FlexWrapper>
-
-      <p>
-        Back 2 is a reachy high attack that is also a reasonable anti-air move.
-        You can combine it with Back 3 - B3 baiting a jump into B2 punishing
-        that jump if the opponent doesn’t time his attack properly. It is also
-        cancelable into any of the special moves as B3, but because of its
-        shorter range it is best used as a combo starter from a jumping-in
-        opponent - B2 F1 for a full spear combo punish.
-      </p>
-
-      <Callout>
-        <div className="text-base font-bold flex items-center gap-2">
-          <GiHighKick /> Practice
-        </div>
-        <p>
-          Set a jump-in practice dummy and try to hit them with B2. Then record
-          the opponent jumping in with a kick attack. This will be harder to
-          counter with B2, but should be practiced. Once you feel confident with
-          the above, try connecting a spear - B2 F1 into whatever combo you
-          prefer.
         </p>
       </Callout>
 
