dbb429b8d436c61f42019733d35d0b37eea0ee7d
Dan Atanasov <danmanm@gmail.com>
2026-03-03 20:33:01 +0200

    Refactor Battle Round page: enhance clarity of game format description; update phase titles for consistency; improve prose in Command and Movement phases; add details on Strategic Reserves and transport mechanics.

--- app/warhammer-40k/battle-round/page.tsx
+++ app/warhammer-40k/battle-round/page.tsx
@@ -18,21 +18,20 @@ export default async function Page() {
 
       <Section>
         <p>
-          A game of WH40k can be played in various formats (mission types, army
-          size limits, etc.), but is always played in a series of rounds. All
-          players take a turn within a single battle round. Each player's turn
-          is divided into a sequence of phases. After all phases are complete,
-          the turn switches.
+          A game of WH40k can be played in various formats, but it's always
+          played in a series of rounds. All players take a turn within a single
+          battle round. Each player's turn is divided into a sequence of phases.
+          After all phases are complete, the turn switches.
         </p>
 
         <div className="box md:text-lg">
-          <span>1. Command Phase</span> &rarr; <span>2. Movement Phase</span>{' '}
-          &rarr; <span>3. Shooting Phase</span> &rarr;{' '}
-          <span>4. Charge Phase</span> &rarr; <span>5. Fight Phase</span>
+          <span>1. Command</span> &rarr; <span>2. Movement</span> &rarr;{' '}
+          <span>3. Shooting</span> &rarr; <span>4. Charge</span> &rarr;{' '}
+          <span>5. Fight</span>
         </div>
       </Section>
 
-      <Section title="Command Phase" id="command-phase">
+      <Section title="1. Command Phase" id="command-phase">
         <p>Every player's turn starts with the Command Phase.</p>
 
         <ol>
@@ -47,7 +46,7 @@ export default async function Page() {
           </li>
 
           <li>
-            The active player only takes <strong>Battle-shock</strong> tests for{' '}
+            The active player takes <strong>Battle-shock</strong> tests for{' '}
             <strong>all their units below half-strength</strong>.
           </li>
         </ol>
@@ -58,8 +57,8 @@ export default async function Page() {
 
         <ul>
           <li>
-            <strong>Single model units</strong> (e.g. characters, vehicles):
-            Less than half wounds left.
+            <strong>Single model</strong> (e.g. characters, vehicles): Less than
+            half wounds left.
           </li>
 
           <li>
@@ -69,8 +68,8 @@ export default async function Page() {
         </ul>
 
         <p>
-          If you have units that fit the description, <strong>roll 2D6</strong>{' '}
-          for each and{' '}
+          If it's your Command Phase and you have units that fit these
+          descriptions, <strong>roll 2D6</strong> for each and{' '}
           <strong>compare the result to their Leadership (Ld)</strong> (the best
           Ld for squads with attached leaders). If the roll is lower than the Ld
           the unit is shocked. That is why <strong>lower Ld is better</strong>.
@@ -79,8 +78,8 @@ export default async function Page() {
         <div className="example">
           <p>
             A 10-man squad of Intercessors is down to 4 marines after the
-            opponent's last turn. Now, when its your Command Phase. They must
-            take the test because its 4 out of 10 models left. You roll 2D6 and
+            opponent's last turn. Now, its your Command Phase and they must take
+            a battle-shocked test: 4 out of 10 models left. You roll 2D6 and
             score a 5. Since Intercessors have Ld of 6+, they fail and are
             shocked.
           </p>
@@ -88,7 +87,7 @@ export default async function Page() {
           <p>
             If it's the same squad with a Chaplain attached, they must still
             take the test: 5 out of 11 models. But because the Chaplain has Ld
-            of 5+, they would pass the same roll.
+            of 5+, they would pass with a roll of 5.
           </p>
         </div>
 
@@ -98,18 +97,18 @@ export default async function Page() {
             its Objective Control (OC) becomes 0 and it cannot be affected by
             Stratagems
           </strong>
-          . This effectively shuts down scoring and support efficiency. One
-          player may lock down all objectives, but if their opponent trades in
-          enough models, they have a chance to swing control.
+          . This shuts down scoring and support efficiency. One player may lock
+          down all objectives, but if their opponent trades in enough models,
+          they have a chance to swing control.
         </p>
       </Section>
 
-      <Section title="Movement Phase" id="movement-phase">
+      <Section title="2. Movement Phase" id="movement-phase">
         <p>
-          The active player goes trough{' '}
+          The active player goes through{' '}
           <strong>all units outside engagement range</strong> (1" horizontal, 5"
-          vertical) of an enemy (outside melee combat), one at a time, an
-          chooses weather they will:
+          vertical) of an enemy, one at a time, an chooses weather the given
+          unit will do one of the following:
         </p>
 
         <ul>
@@ -120,7 +119,7 @@ export default async function Page() {
 
           <li>
             <strong>Advance:</strong> Roll D6 and add the result as inches to
-            its Movement at the cost of limited shooting capabilities during the
+            its M" at the cost of limited shooting capabilities during the
             following phase.
           </li>
 
@@ -136,6 +135,15 @@ export default async function Page() {
             trough enemies.
           </strong>{' '}
           Units that already were engaged can either stay put, or fall back.
+        </p>
+
+        <p>
+          Also, models{' '}
+          <strong>cannot end a move on top of an objective marker</strong> under
+          core rules, and markers have no defined shape. However,{' '}
+          <strong>tournaments</strong> standardize objectives as{' '}
+          <strong>40mm circular markers</strong> and allow models to end their
+          movement on them.
         </p>
 
         <h3 id="fall-back">Fall Back</h3>
@@ -168,21 +176,58 @@ export default async function Page() {
         <h3 id="transports">Transports</h3>
 
         <p>
-          Some units may carry other units within them, usually for the benefit
-          of much higher M". A unit can{' '}
+          Some units may carry other units within them. The benefit is that the
+          transports usually has much higher mobility and durability. The risk
+          is that it may be destroyed while delivering its payload.
+        </p>
+
+        <p>
+          A unit can{' '}
           <strong>
             embark if every model ends a move wholly within 3" of the transport
           </strong>
           . Then the transport carries them to their destination where they can
-          disembark. If they disembark durin a turn where the transport has
-          moved, they can only shoot. Otherwise they act as normal.
+          disembark. If they <strong>disembark</strong> during a turn where the{' '}
+          <strong>transport has already moved, they can only shoot</strong>.
+          Otherwise they can also move.
         </p>
 
         <p>
           If the <strong>transport is destroyed</strong>, roll D6 per embarked
-          model (not unit). On a 1 the model suffers a mortal wound. Then the
-          survivors disembark and become Battle-shocked and cannot charge that
-          turn.
+          model. On a 1 the model suffers a mortal wound. Then the survivors
+          disembark and become Battle-shocked and cannot charge that turn.
+        </p>
+
+        <h3 id="strategic-reserves">Strategic Reserves</h3>
+
+        <p>
+          Both players may choose{' '}
+          <strong>not to deploy some of their units</strong> at the begining of
+          the battle. These go into Strategic Reserves and{' '}
+          <strong>cannot exceeed 25% of the total army points</strong>.
+        </p>
+
+        <p>
+          Reserved units may{' '}
+          <strong>
+            join the battle, only at the very end of the movement phase
+          </strong>
+          , after all other units have moved or stayed put, folling these rules:
+        </p>
+
+        <ul>
+          <li>All units must deploy more than 9" from enemy units.</li>
+
+          <li>
+            Round 2: Within 6" of a table edge, not in enemy deployment zone.
+          </li>
+
+          <li>Round 3+: Within 6" of any table edge.</li>
+        </ul>
+
+        <p>
+          Reserve play is about timing and pressure. Late arrivals can threaten
+          flanks or objectives but must respect screening and positioning.
         </p>
       </Section>
     </>
