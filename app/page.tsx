84ad2126bc3448e84b9f5267bcdd1dee91bb9a8a
Dan Atanasov <danmanm@gmail.com>
2026-03-29 21:36:36 +0300

    refactor: update styles and layout in globals.css and page.tsx for improved design and responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,29 @@
 import Link from 'next/link'
 import { FaAsterisk } from 'react-icons/fa'
+import { Gi3dStairs } from 'react-icons/gi'
+import { ImConfused } from 'react-icons/im'
+import { RiBookShelfLine } from 'react-icons/ri'
 
 import { ROOT_NAVIGATION_ITEMS } from '@/app/constants'
-import { Callout } from '@/app/ui/Callout'
-import { Section } from '@/app/ui/Section'
 
 export default function Page() {
   return (
-    <main id="main-content" tabIndex={-1} className="px-6 py-12 space-y-16">
-      <Section className="max-w-3xl mx-auto space-y-16 text-center h-[calc(100dvh-(--spacing(32)))] flex flex-col justify-around items-center">
-        <div>
-          <div className="text-5xl md:text-7xl text-primary font-bold animate-slide-left">
-            Docs
+    <main
+      id="main-content"
+      tabIndex={-1}
+      className="edge-padding py-12 md:py-0"
+    >
+      <section className="max-w-3xl mx-auto space-y-20 h-dvh flex flex-col justify-around">
+        <div className="space-y-2 md:space-y-4 font-bold text-center">
+          <div className="text-4xl md:text-6xl animate-slide-left">
+            The <span className="text-primary">Casual Meta</span>
           </div>
 
-          <div className="text-4xl md:text-5xl animate-slide-right">
-            for <span className="italic border-b">casual</span>{' '}
+          <div
+            className="text-2xl md:text-4xl animate-slide-right"
+            style={{ animationDelay: '0.3s' }}
+          >
+            Docs for <span className="italic border-b">casual</span>{' '}
             <sup>
               <FaAsterisk className="inline text-primary" />
             </sup>{' '}
@@ -23,60 +31,70 @@ export default function Page() {
           </div>
         </div>
 
-        <div className="flex-wrapper justify-center font-semibold text-xl">
-          {ROOT_NAVIGATION_ITEMS.map((item, index) => (
-            <Link
-              key={item.href}
-              href={item.href}
-              style={{ animationDelay: `${0.3 + index * 0.3}s` }}
-              className="animate-fade-in-scale px-4 py-2 hover:text-primary transition underline"
-            >
-              {item.name}
-            </Link>
-          ))}
+        <div className="space-y-4">
+          <div
+            style={{ animationDelay: '0.6s' }}
+            className="animate-fade-in-scale text-xl font-bold text-center"
+          >
+            What drives your interest?
+          </div>
+
+          <div className="flex-list text-lg justify-center">
+            {ROOT_NAVIGATION_ITEMS.map((item, index) => (
+              <Link
+                key={item.href}
+                href={item.href}
+                style={{ animationDelay: `${0.9 + index * 0.3}s` }}
+                className="animate-fade-in-scale"
+              >
+                {item.name}
+              </Link>
+            ))}
+          </div>
         </div>
 
-        <Callout className="animate-fade-in-scale delay-75 px-4">
-          <div className="text-xl">
-            <sup>
+        <div
+          className="box animate-fade-in-scale divide-y-2 divide-foreground/10"
+          style={{ animationDelay: '1.2s' }}
+        >
+          <p className="text-xl pb-4">
+            <sup className="text-xs">
               <FaAsterisk className="inline text-primary mr-2" />
             </sup>
             Games can be <em>fun</em>. They can be <em>hard</em>. They can be{' '}
             <strong>both</strong> as well.
+          </p>
+
+          <div className="space-y-4">
+            <div className="space-y-4 md:flex md:gap-4">
+              <div className="flex flex-col items-center text-center">
+                <ImConfused className="text-2xl" />
+                <div>Want to get into a game, but feel intimidated?</div>
+              </div>
+
+              <div className="flex flex-col items-center text-center">
+                <Gi3dStairs className="text-2xl" />
+                <div>
+                  Are playing, but feel a lack of progress or connection?
+                </div>
+              </div>
+
+              <div className="flex flex-col items-center text-center">
+                <RiBookShelfLine className="text-2xl" />
+                <div>
+                  Like to explore, but are overwhelmed by the amount of
+                  information?
+                </div>
+              </div>
+            </div>
+
+            <p>
+              The Casual Meta consolidates community knowledge from multiple
+              sources as simple ideas.
+            </p>
           </div>
-        </Callout>
-      </Section>
-
-      <article>
-        <Section className="max-w-3xl mx-auto">
-          <p>
-            The Casual Meta brings together community knowledge from YouTube,
-            Reddit, official and unofficial guides, forums and wikis. The goal
-            is to extract simple ideas from the multitude of sources out there,
-            so you can focus on improving. The articles here are designed for{' '}
-            <em>players</em> who:
-          </p>
-
-          <ul role="list">
-            <li>Want to get into a game, but feel intimidated;</li>
-            <li>
-              Are playing a game for some time, but feel a lack of progress or
-              connection;
-            </li>
-            <li>
-              Like to explore concepts, but are overwhelmed by the amount of
-              information available.
-            </li>
-          </ul>
-
-          <p>
-            META means "most effective tactics available". This often refers to
-            techniques which are challenging for casual players. Hence the name
-            Casual Meta infers the pivot on consistency over the "best possible"
-            outcoume.
-          </p>
-        </Section>
-      </article>
+        </div>
+      </section>
     </main>
   )
 }
