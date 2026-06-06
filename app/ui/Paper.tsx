958fe444465af170baf4c9cc1a2a1cce345192f3
Dan Atanasov <danmanm@gmail.com>
2026-06-07 00:05:08 +0300

    refactor: enhance Paper and Strip components; update utility styles and improve attack sequence page content for clarity

--- app/ui/Paper.tsx
+++ app/ui/Paper.tsx
@@ -11,7 +11,7 @@ export const Paper: React.FC<
     {icon &&
       React.createElement(icon, {
         className:
-          'text-9xl absolute text-foreground/6 top-0 right-0 -z-1 rotate-20',
+          'text-9xl absolute text-foreground/6 top-0 right-0 -z-1 rotate-20 ',
       })}
 
     {title && <p className="font-bold uppercase">{title}</p>}
--- app/warhammer-40k/attack-sequence/page.tsx
+++ app/warhammer-40k/attack-sequence/page.tsx
@@ -1,16 +1,13 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import { IoDiceOutline } from 'react-icons/io5'
 
 import attack from '@/app/assets/wh40k/attack.webp'
 import { wh40kHref, wh40kMetadata, wh40kPage } from '@/app/siteMap'
 import { OrDivider } from '@/app/ui/OrDivider'
+import { Strip } from '@/app/ui/Strip'
 import { Table } from '@/app/ui/Table'
 import { woundRollColumns, woundRollRows } from '@/app/warhammer-40k/constants'
-import { Paper } from '@/app/ui/Paper'
-import { LuDices } from 'react-icons/lu'
-import { Strip } from '@/app/ui/Strip'
-import { GiRollingDices } from 'react-icons/gi'
+import { GiPerspectiveDiceSixFacesFive } from 'react-icons/gi'
 
 const page = wh40kPage('attackSequence')
 
@@ -38,9 +35,9 @@ export default async function Page() {
         <div className="flex-center">
           <div>
             <div className="font-bold uppercase">Weapon</div>
-            <div>Strength</div>
-            <div>AP</div>
-            <div>Damage</div>
+            <div>Strength (S)</div>
+            <div>Armor Penetration (AP)</div>
+            <div>Damage (D)</div>
           </div>
 
           <div>
@@ -52,9 +49,9 @@ export default async function Page() {
 
           <div>
             <div className="font-bold uppercase">Target</div>
-            <div>Toughness</div>
-            <div>Saves</div>
-            <div>Wounds</div>
+            <div>Toughness (T)</div>
+            <div>Saves (Sv)</div>
+            <div>Wounds (W)</div>
           </div>
         </div>
 
@@ -63,8 +60,8 @@ export default async function Page() {
           unit <strong>has to be in range of the target</strong> to declare an
           attack - engagement (1") if melee, or within the Range attribute of
           the weapon if ranged. Then the attacking weapon does{' '}
-          <strong>as many activations as it's Attacks atrribute</strong> states.
-          Both ranged and melee weapons go through the same sequence:
+          <strong>as many activations as it's Attacks (A) atrribute</strong>{' '}
+          states. Both ranged and melee weapons go through the same sequence:
         </p>
 
         <ol>
@@ -74,55 +71,48 @@ export default async function Page() {
           <li>Roll to save - either armor or invulnerable.</li>
           <li>Inflict damage.</li>
         </ol>
+      </section>
 
+      <section>
         <h2 id="hit-roll">1. Roll to hit</h2>
 
-        <Paper icon={LuDices}>
-          <Strip
-            items={[
-              {
-                label: 'Roll',
-
-                value: (
-                  <>
-                    <p>
-                      D6 for each activation each weapon does{' '}
-                      <GiRollingDices className="inline text-2xl" />{' '}
-                    </p>
-                  </>
-                ),
-              },
-              {
-                label: 'Check if',
-
-                value: (
-                  <p>
-                    Result is higher than weapon's{' '}
-                    <Link
-                      href={wh40kHref(
-                        'datasheets',
-                        'ballistic-skill-weapon-skill',
-                      )}
-                    >
-                      WS/BS
-                    </Link>
-                    .
-                  </p>
-                ),
-              },
-              {
-                label: 'On success',
-
-                value: <p>The attack hits its target.</p>,
-              },
-              {
-                label: 'On fail',
-
-                value: <p>The attack misses its target.</p>,
-              },
-            ]}
-          />
-        </Paper>
+        <Strip
+          items={[
+            {
+              label: 'Roll',
+              value: (
+                <>
+                  <p>D6 for each activation each weapon does</p>
+                </>
+              ),
+            },
+            {
+              label: 'Check if',
+              value: (
+                <p>
+                  Result is higher than weapon's{' '}
+                  <Link
+                    href={wh40kHref(
+                      'datasheets',
+                      'ballistic-skill-weapon-skill',
+                    )}
+                  >
+                    WS/BS
+                  </Link>
+                  .
+                </p>
+              ),
+            },
+            {
+              label: 'On success',
+              value: <p>The attack hits its target.</p>,
+            },
+            {
+              label: 'On fail',
+              value: <p>The attack misses its target.</p>,
+            },
+          ]}
+        />
 
         <p>
           An{' '}
@@ -135,89 +125,253 @@ export default async function Page() {
           </Link>
           .
         </p>
+      </section>
 
+      <section>
         <h2 id="wound-roll">2. Roll to wound</h2>
 
-        <blockquote>
-          <p>
-            <IoDiceOutline className="inline" /> Roll a <strong>D6</strong> only
-            for the attacks that did connect. The result follows a table based
-            on how much higher or lower the weapon's{' '}
-            <Link href={wh40kHref('datasheets', 'strength')}>Strength (S)</Link>{' '}
-            is compared to the target's{' '}
-            <Link href={wh40kHref('datasheets', 'toughness')}>
-              Toughness (T)
-            </Link>
-            .
-          </p>
-        </blockquote>
-
-        <p>
-          An unmodified roll of 6 is a <strong>critical wound</strong>.
-        </p>
+        <Strip
+          items={[
+            {
+              label: 'Roll',
+              value: (
+                <>
+                  <p>D6 for each attack that hit.</p>
+                </>
+              ),
+            },
+            {
+              label: 'Compare',
+              value: (
+                <p>
+                  Weapon's S against the target's T using the following table.
+                  Then check the result.
+                </p>
+              ),
+            },
+            {
+              label: 'On success',
+              value: <p>The attack wounds its target.</p>,
+            },
+            {
+              label: 'On fail',
+              value: <p>The attack fails to deal damage.</p>,
+            },
+          ]}
+        />
 
         <Table columns={woundRollColumns} data={woundRollRows} />
 
+        <p>
+          <strong>
+            An unmodified roll of 6 is a <em>critical wound</em>
+          </strong>
+          .
+        </p>
+      </section>
+
+      <section>
         <h2 id="allocate-wounds">3. Allocate wounds</h2>
 
         <p>
           If the attack hits and wounds, the{' '}
           <strong>defender allocates which models take the wounds</strong>{' '}
           (unless the attacking weapon has{' '}
-          <Link href={wh40kHref('datasheets', 'precision')}>precision</Link>
+          <Link href={wh40kHref('datasheets', 'precision')}>Precision</Link>
           ). Models that already lost wounds or had attacks allocated this phase
-          must be selected first. Saves represent the target's armor.
+          must be selected first.
         </p>
+      </section>
 
+      <section>
         <h2 id="save-roll">4. Roll to save</h2>
 
-        <blockquote>
-          <p className="font-bold">Armor save</p>
+        <p>
+          Saves represent the target's armor. They are the third and final gate
+          against taking damage. Once wounds are allocated, the selected models
+          roll for saves. Some units have <em>Invulnerable saves</em> in
+          addition to their regular save. The defender chooses which one to roll
+          for. They can't roll for both.
+        </p>
 
-          <p>
-            <IoDiceOutline className="inline" /> Roll a <strong>D6</strong> for
-            each allocated wound. Modify the roll by the{' '}
-            <Link href={wh40kHref('datasheets', 'armor-penetration')}>
-              Armor Penetration (AP)
-            </Link>{' '}
-            of the weapon. If the result is equal to or greater than the
-            target's Save (Sv), damage is not inflicted.
-          </p>
-        </blockquote>
+        <p className="font-bold uppercase">Armor save (Sv)</p>
+
+        <Strip
+          items={[
+            {
+              label: 'Roll',
+              value: (
+                <>
+                  <p>D6 for each allocated attack that wounds.</p>
+                </>
+              ),
+            },
+            {
+              label: 'Modify',
+              value: (
+                <p>
+                  Add the weapon's{' '}
+                  <Link href={wh40kHref('datasheets', 'armor-penetration')}>
+                    AP
+                  </Link>{' '}
+                  to the roll. It's usually a negative number, so it reduces the
+                  result.
+                </p>
+              ),
+            },
+            {
+              label: 'Check if',
+              value:
+                "Modified result is equal to or greater than the target's Save (Sv).",
+            },
+            {
+              label: 'On success',
+              value: 'Damage is averted.',
+            },
+            {
+              label: 'On fail',
+              value: 'Target eats the damage.',
+            },
+          ]}
+        />
 
         <OrDivider />
 
-        <blockquote>
-          <p className="font-bold">Invulnerable save</p>
-          <p>
-            <IoDiceOutline className="inline" /> Roll a <strong>D6</strong> for
-            each allocated wound. If the result is equal to or greater than the
-            target's Invulnerable Save, damage is not inflicted, but you lose
-            the ability to roll for a regular save.
-          </p>
-        </blockquote>
+        <p className="font-bold uppercase">Invulnerable save (Inv)</p>
 
-        <p>If you are the defender</p>
-
-        <h2 id="damage">Inflict damage</h2>
+        <Strip
+          items={[
+            {
+              label: 'Roll',
+              value: 'D6 for each allocated attack that wounds.',
+            },
+            {
+              label: 'Check if',
+              value:
+                "Result is equal to or greater than the target's Invulnerable Save (Sv).",
+            },
+            {
+              label: 'On success',
+              value: 'Damage is averted.',
+            },
+            {
+              label: 'On fail',
+              value: 'Target eats the damage.',
+            },
+          ]}
+        />
 
         <p>
-          After all three rolls are done, if the attack hits, wounds, and the
-          armor save fails, damage is inflicted. The target loses wounds equal
-          to the{' '}
-          <Link href={wh40kHref('datasheets', 'damage')}>Damage (D)</Link> of
-          the weapon. If that damage is more than is required to kill the target
-          model, the excess cannot be allocated to other targets and is lost.
+          Invulnerable saves are better than regular saves when facing high-AP
+          weapons.
+        </p>
+
+        <p className="example">
+          For example, the Terminators already have an optimal Sv2+, so they're
+          pretty safe from most weapons. But an attack from an AP-4 Meltagun,
+          will reduce the Sv to 6+. In that case, the Terminators would fare
+          better if they use their Inv4+ instead.
+        </p>
+      </section>
+
+      <section>
+        <h2 id="preventing-damage">Feel no Pain (FNP)</h2>
+
+        <p>
+          A small amount of units have an additional gate called Feel No Pain
+          x+, that involves yet another dice roll.
+        </p>
+
+        <Strip
+          items={[
+            {
+              label: 'Roll',
+              value: (
+                <p>
+                  D6{' '}
+                  <GiPerspectiveDiceSixFacesFive className="inline text-2xl" />
+                </p>
+              ),
+            },
+            {
+              label: 'Check if',
+              value: "Result is equal to or greater than the FNP's x+.",
+            },
+            {
+              label: 'On success',
+              value: 'Damage is averted.',
+            },
+          ]}
+        />
+
+        <p>
+          Less than 6% of all datasheets mention FNP in some way, and most of
+          the units that can use it, activate it conditionally. It's rare.
+        </p>
+      </section>
+
+      <section>
+        <h2 id="damage">5. Inflict damage</h2>
+
+        <p>
+          After all three rolls are done, only the attacks that hit, wound, and
+          aren't prevented by any saves, inflict damage. The target loses Wounds
+          (W) equal to the weapon's{' '}
+          <Link href={wh40kHref('datasheets', 'damage')}>Damage (D)</Link>. If
+          that damage is more than is required to kill the target, the{' '}
+          <strong>
+            excess cannot be allocated to other targets and is lost
+          </strong>
+          .
+        </p>
+      </section>
+
+      <section>
+        <h2 id="probability">Probability</h2>
+
+        <p>
+          The attack sequence is a series of independent probability gates -
+          hit, wound, save, and potentially FNP. Each roll risks the failure of
+          the whole activation.{' '}
+          <strong>
+            Even theoretically perfect attacks (2+ to hit, 2+ to wound, no save)
+            land roughly 69% of the time.
+          </strong>{' '}
+          The system guarantees that no single attack is ever guaranteed.
         </p>
 
         <Image src={attack} alt="Attack sequence example" className="my-6" />
 
-        <p>
-          The attack sequence is a series of independent probability gates. Each
-          roll is a chance for the whole attack to fail, so even a theoretically
-          perfect attack (2+ to hit, 2+ to wound, no save) only lands roughly
-          69% of the time. The system is designed so that no single attack is
-          ever guaranteed.
+        <p className="example">
+          For example, a 5-man Intercessor squad declares ranged attacks with
+          their Bolt Rifles againts a squad of Ork Boyz. The Bolt Rifles do 2
+          attacks each, but Intercessors have a focus fire ability that doubles
+          the attacks. That is <strong>20 attacks</strong> in total. The
+          attacker rolls 20 dice.
+        </p>
+
+        <p className="example">
+          Bolt Rifle has BS3+. That's a 2/3 chance, so let's say the attacker
+          scores 13 hits (about as expected) in this particular roll.
+        </p>
+
+        <p className="example">
+          Next, is ther roll to wound. The Boyz are a tough nut (T5) for the
+          Bolt Rifle (S4). Attacks wound on 5+ (1/3 chance). Let's say, the
+          attacker rolls 13 dice and scores above the average with 5 wounding
+          attacks.
+        </p>
+
+        <p className="example">
+          The good news is that Boyz have an already poor Sv of 5+, reduced to
+          6+ by the Bolt Rifle's AP-1. The defender thows 5 dice and gets 1
+          save.
+        </p>
+
+        <p className="example">
+          In the end, out of 20 attacks, 4 deal damage. The Bolt Rifle is D2,
+          while the Boyz are W1. So, 4 Boyz are removed from play. The excess 1
+          damage is lost.
         </p>
       </section>
     </>
